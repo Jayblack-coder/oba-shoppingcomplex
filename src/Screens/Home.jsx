25b81cc2c6f3d5f00531134195758cb01541a1f5
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2026-01-13 09:46:52 +0000

    home

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -54,10 +54,11 @@ const contentWrapper = {
 
 
       {/* HERO SECTION */}
-      {/* <Box sx={{ px: { xs: 2, md: 8 }, py: 10, textAlign: "center" }}> */}
-      {/* <Box sx={{ py: { xs: 6, md: 8 } }}> */}
-      <Box sx={contentWrapper} py={{ xs: 6, md: 8 }}  textAlign="center" justifyContent="center"
-  alignItems="center">
+      <Box sx={{ px: { xs: 2, md: 8 }, py: 10, textAlign: "center" }} justifyContent="center"
+  alignItems="center"> 
+      <Box sx={{ py: { xs: 6, md: 8 } }}>
+      {/* <Box sx={contentWrapper} py={{ xs: 6, md: 8 }}  textAlign="center" justifyContent="center"
+  alignItems="center"> */}
 
 
         <Typography variant="h3" fontWeight="bold" gutterBottom color="#da7127">
@@ -131,7 +132,7 @@ const contentWrapper = {
           </Button>
         </Stack>
       </Box>
-      {/* </Box> */}
+      </Box>
 {/* <Divider sx={{ my: 8, borderColor: "rgba(139, 69, 19, 0.15)", maxWidth: "85%", mx: "auto" }} /> */}
 <Divider
   sx={{
