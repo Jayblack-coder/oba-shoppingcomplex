aeb85a257d087ed71909c7595061afbfb30c5006
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2026-01-13 09:39:42 +0000

    home

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -56,7 +56,7 @@ const contentWrapper = {
       {/* HERO SECTION */}
       {/* <Box sx={{ px: { xs: 2, md: 8 }, py: 10, textAlign: "center" }}> */}
       {/* <Box sx={{ py: { xs: 6, md: 8 } }}> */}
-      <Box sx={contentWrapper} py={{ xs: 4, md: 5 }}>
+      <Box sx={contentWrapper} py={{ xs: 4, md: 5 }}  textAlign="center">
 
 
         <Typography variant="h3" fontWeight="bold" gutterBottom color="#da7127">
